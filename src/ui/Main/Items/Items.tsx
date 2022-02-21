2fe0422f1412888b8824d2b3051ee769ba4ab7d0
Yauheni Kapachenia <kapachenia@gmail.com>
2022-02-21 23:42:49 +0300

    added pagination

--- src/ui/Main/Items/Items.tsx
+++ src/ui/Main/Items/Items.tsx
@@ -1,16 +1,33 @@
 import React from "react";
-import {useSelector} from "react-redux";
-import {itemType} from "../../../bll/setReducer";
+import {useDispatch, useSelector} from "react-redux";
+import {itemType, setCurrencyTC} from "../../../bll/setReducer";
 import {AppRootStoreType} from "../../../bll/store";
 import {Item} from "./Item/Items";
 
 export const Items = () => {
 
+    const dispatch = useDispatch()
 
     const itemCurrency = useSelector<AppRootStoreType, Array<itemType>>(state => state.setReducer.data)
+    const pageSize = useSelector<AppRootStoreType, number>(state => state.setReducer.pageSize)
+    const totalPage = useSelector<AppRootStoreType, number>(state => state.setReducer.totalPage)
+    // const currentPage = useSelector<AppRootStoreType, number>(state => state.setReducer.currentPage)
 
+    // const setHandler = () => {
+    //     console.log('click')
+    //     dispatch(setCurrencyTC())
+    // }
 
+    const setPagination = (p: number) => {
+        console.log(p)
+        dispatch(setCurrencyTC(p * 10))
+    }
 
+    let pagesCount = Math.ceil(totalPage / pageSize)
+    let pages = []
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i)
+    }
 
     return (
         <div>
@@ -18,10 +35,10 @@ export const Items = () => {
             <div>
                 <table>
                     <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Наименование</th>
-                        </tr>
+                    <tr>
+                        <th>#</th>
+                        <th>Наименование</th>
+                    </tr>
                     </thead>
                     {itemCurrency.map(m => (<Item
                         key={m.id}
@@ -30,7 +47,9 @@ export const Items = () => {
                     />))}
                 </table>
             </div>
-
+            {pages.map((p, index) => {
+                return <span key={index} onClick={() => setPagination(p)}>{p}</span>
+            })}
         </div>
     )
 }

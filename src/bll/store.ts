3344688e9119f7c995848743d17ac9164e23a515
Yauheni Kapachenia <kapachenia@gmail.com>
2022-02-28 18:12:33 +0300

    removed files for development

--- src/bll/store.ts
+++ src/bll/store.ts
@@ -20,6 +20,3 @@ localStorage.setItem('state', JSON.stringify(store.getState().cart.currencyInBri
 
 
 export type AppRootStoreType = ReturnType<typeof rootReducer>
-
-// @ts-ignore
-window.store = store; // for dev
